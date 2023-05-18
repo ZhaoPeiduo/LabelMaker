54197dd4871c8dc440f5c913feaa95725999b674
Zhao Peiduo <peiduo.zhao@seventhsense.ai>
2023-05-18 17:42:15 +0800

    Add support for multi labels

--- FrameWidgets.py
+++ FrameWidgets.py
@@ -6,9 +6,10 @@ import os
 
 class FrameWidgets:
     KEYMAP = {chr(i): i - 48 if i < 58 else i - 55 if i < 91 else i - 61 for i in range(48, 91)}
-    def __init__(self, window, manager: Manager) -> None:
+    def __init__(self, window, manager: Manager, mode) -> None:
         self.window = window
         self.manager = manager
+        self.mode=  mode
 
         self.control_frame = tk.Frame(self.window)
         self.control_frame.pack()
@@ -26,6 +27,9 @@ class FrameWidgets:
         self.exit_button = tk.Button(self.control_frame, text="Exit", command=self.exit)
         self.window.bind("<Escape>", lambda event: self.exit_button.invoke())
         self.exit_button.pack(side=tk.LEFT, padx=5)
+
+        if mode == "multi":
+            self.window.bind("<Return>", lambda event: self.confirm())
 
         self.image_label = tk.Label(self.window)
         self.image_label.pack()
@@ -64,6 +68,10 @@ class FrameWidgets:
     def read_labels(self):
         label_file = filedialog.askopenfilename(title="Select file containing possible labels")
         self.manager.load_labels(label_file)
+        if self.mode == "multi":
+            self.label_buffer = {key : value for key, value in zip(self.manager.possible_labels, 
+                                                                   [0 for _ in range(len(self.manager.possible_labels))])}
+            print(self.label_buffer)
  
     def save_labels(self):
         save_file = filedialog.asksaveasfile(title="Select location to save labels")
@@ -81,17 +89,40 @@ class FrameWidgets:
 
     def undo(self):
         self.manager.index -= 1
-        decrement = self.manager.possible_labels.index(self.manager.labels.pop(-1))
-        self.manager.counters[decrement].set(self.manager.counters[decrement].get() - 1)
+        if self.mode == "single":
+            decrement = self.manager.possible_labels.index(self.manager.labels.pop(-1))
+            self.manager.counters[decrement].set(self.manager.counters[decrement].get() - 1)
+        else:
+            for label, value in self.manager.labels.pop(-1).items():
+                print(self.manager.labels)
+                decrement = self.manager.possible_labels.index(label)
+                self.manager.counters[decrement].set(self.manager.counters[decrement].get() - 1)
+        self.display_data(self.manager.image_paths[self.manager.index])
+    
+    def confirm(self):
+        self.manager.labels.append(self.label_buffer)
+        self.label_buffer = {key : value for key, value in zip(self.manager.possible_labels, 
+                                                                   [0 for _ in range(len(self.manager.possible_labels))])}
+        self.manager.index += 1
+        if self.manager.index == self.manager.image_count:
+            self.save_labels()
+            return
+        
         self.display_data(self.manager.image_paths[self.manager.index])
 
     def assign_label(self, index):
         if self.manager.index == self.manager.image_count:
             self.save_labels()
             return
-        self.manager.labels.append(self.manager.possible_labels[index])
-        self.manager.counters[index].set(self.manager.counters[index].get() + 1)
-        self.manager.index += 1
-        if self.manager.index < self.manager.image_count:
+        
+        label_to_add = self.manager.possible_labels[index]
+        if self.mode == "single":
+            self.manager.labels.append(label_to_add)
+            self.manager.counters[index].set(self.manager.counters[index].get() + 1)
+            self.manager.index += 1
             self.display_data(self.manager.image_paths[self.manager.index])
+        elif self.label_buffer[label_to_add] == 0:
+            self.manager.counters[index].set(self.manager.counters[index].get() + 1)
+            self.label_buffer[label_to_add] += 1
+        
    
--- labelmaker.py
+++ labelmaker.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 from Manager import Manager
 from FrameWidgets import FrameWidgets
+import argparse
 
 class DataLabeler:
-    def __init__(self):
+    def __init__(self, mode):
         self.window = tk.Tk()
         self.window.title("Manual Data Labeler")
 
         self.manager = Manager()
-        self.frame_widgets = FrameWidgets(self.window, self.manager)
+        self.frame_widgets = FrameWidgets(self.window, self.manager, mode)
 
     def run(self):
         self.frame_widgets.read_images()
@@ -17,7 +18,18 @@ class DataLabeler:
         self.window.mainloop()
 
 def main():
-    labeler = DataLabeler()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--mode",
+        "-m",
+        help= "Single (Binary or Categorical Label) or Multi-label mode",
+        choices= ['single', 'multi'],
+        default= "single"
+    )
+    args = parser.parse_args()
+
+    labeler = DataLabeler(args.mode)
     labeler.run()
 
 if __name__ == "__main__":
